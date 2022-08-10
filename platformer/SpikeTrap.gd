dfd15e9fc8bcab6c43a73edefc9a1e94bb21e5ac
luctho27 <100096997+luctho27@users.noreply.github.com>
2022-08-10 13:20:16 +1200

    gc

--- platformer/SpikeTrap.gd
+++ platformer/SpikeTrap.gd
@@ -6,5 +6,8 @@ extends Area2D
 
 
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("Player"):
+	print(body)
+	if body.is_in_group("Player"): 
 		print("player dead")
+		if GameStats.check_reset() == false:
+			body.global_position = GameStats.get_spawn().global_position
---
+++ platformer/world.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+	if Input.is_action_just_pressed("reset"):
+		GameStats.reset()
+	print(GameStats.get_time())
+	$CanvasLayer/Control/Label.text = GameStats.get_time()
+
+
